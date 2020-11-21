7bb25507e050419c84d380c2d0f882ef96b44971
Andy Bond <andybond006@gmail.com>
2020-11-20 22:22:09 -0500

    feat: add random selection

--- src/app/hangry/hangry.component.ts
+++ src/app/hangry/hangry.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 import { fade } from '@core/helpers';
 import { DecisionService, MealService, RestaurantService } from './services';
 import { Action, Decision, Meal, Restaurant } from './models';
 import { DecisionComponent } from './decision/decision.component';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-hangry',
@@ -14,11 +15,20 @@ import { DecisionComponent } from './decision/decision.component';
 })
 export class HangryComponent implements OnInit, AfterViewInit, OnDestroy {
   private destroy$ = new Subject<void>();
-  @ViewChildren(DecisionComponent, { read: ElementRef }) elements: QueryList<ElementRef>;
+  @ViewChildren(DecisionComponent, { read: ElementRef }) decisionElements: QueryList<ElementRef>;
+  @ViewChildren(MatCard, { read: ElementRef }) extraElements: QueryList<ElementRef>;
 
   decisions$: Observable<Decision[]>;
+
+  randomMeal$ = new BehaviorSubject<boolean>(false);
+  mealCategory$ = new BehaviorSubject<string>(undefined);
   meals$: Observable<Meal[]>;
+  highlightedMeal: Meal;
+
+  randomRestaurant$ = new BehaviorSubject<boolean>(false);
+  restaurantCategory$ = new BehaviorSubject<string>(undefined);
   restaurants$: Observable<Restaurant[]>;
+  highlightedRestaurant: Restaurant;
 
   meals = false;
   restaurants = false;
@@ -31,17 +41,45 @@ export class HangryComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnInit(): void {
     this.decisions$ = this.decision.decisions$;
-    this.meals$ = this.meal.meals$;
-    this.restaurants$ = this.restaurant.restaurants$;
+    this.meals$ = combineLatest([
+      this.meal.meals$,
+      this.mealCategory$,
+      this.randomMeal$
+    ]).pipe(
+      map(([meals, filter, random]) => {
+        const result = filter ? meals.filter(m => m.categories.includes(filter)) : meals;
+        this.highlightedMeal = random ? result[Math.floor(Math.random() * result.length)] : undefined;
+        return result;
+      })
+    );
+    this.restaurants$ = combineLatest([
+      this.restaurant.restaurants$,
+      this.restaurantCategory$,
+      this.randomRestaurant$
+    ]).pipe(
+      map(([restaurants, filter, random]) => {
+        const result = filter ? restaurants.filter(r => r.categories.includes(filter)) : restaurants;
+        this.highlightedRestaurant = random ? result[Math.floor(Math.random() * result.length)] : undefined;
+        return result;
+      })
+    );
   }
 
   ngAfterViewInit() {
-    this.elements?.changes.pipe(
+    this.decisionElements?.changes.pipe(
       takeUntil(this.destroy$)
     ).subscribe(() => {
-      if (this.elements?.last) {
-        const element = this.elements.last.nativeElement;
-        setTimeout(() => element.scrollIntoView({ block: 'center', behavior: 'smooth' }), 500);
+      if (this.decisionElements?.last) {
+        const element = this.decisionElements.last.nativeElement;
+        setTimeout(() => element.scrollIntoView({ block: 'center', behavior: 'smooth' }), 0);
+      }
+    });
+    this.extraElements?.changes.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      if (this.extraElements?.last) {
+        const element = this.extraElements.last.nativeElement;
+        setTimeout(() => element.scrollIntoView({ block: 'center', behavior: 'smooth' }), 0);
       }
     });
   }
@@ -52,17 +90,40 @@ export class HangryComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   perform(action: Action) {
-    console.log(action);
     if (action.type === 'meal-options') {
       this.restaurants = false;
+      this.restaurantCategory$.next(undefined);
       this.meals = true;
+      this.mealCategory$.next(action.value);
+      this.randomMeal$.next(false);
     } else if (action.type === 'restaurant-options') {
       this.meals = false;
+      this.mealCategory$.next(undefined);
+      this.randomMeal$.next(false);
       this.restaurants = true;
+      if (action.value !== 'random') {
+        this.restaurantCategory$.next(action.value);
+        this.randomRestaurant$.next(false);
+      } else {
+        this.restaurantCategory$.next(undefined);
+        this.randomRestaurant$.next(true);
+      }
     } else {
       console.log('both false');
       this.meals = false;
+      this.mealCategory$.next(undefined);
+      this.randomMeal$.next(false);
       this.restaurants = false;
+      this.restaurantCategory$.next(undefined);
+      this.randomRestaurant$.next(false);
+    }
+  }
+
+  random(type: string) {
+    if (type === 'meal') {
+      this.randomMeal$.next(true);
+    } else if (type === 'restaurant') {
+      this.randomRestaurant$.next(true);
     }
   }
 }
